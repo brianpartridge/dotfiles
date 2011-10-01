17765e78ced669d5262a922aca21482c9898ac1c
Brian Partridge <brianpartridge@gmail.com>
2011-10-01 18:40:02 -0400

    added exception logging when copying

--- bin/torrent-finished.py
+++ bin/torrent-finished.py
@@ -78,8 +78,12 @@ class Torrent(object):
 
 		if dest:
 			logging.info("Copying %s to %s" % (self.mediaFilePath, dest))
-			shutil.copy(self.mediaFilePath, dest)
-			logging.info("Copy complete")
+			try:
+				shutil.copy(self.mediaFilePath, dest)
+			except:
+				logging.exception("Error while copying %s to %s" % (self.mediaFilePath, dest))
+			else:
+				logging.info("Copy complete")
 		else:
 			logging.info("Not adding %s to media library", self.name)
 		pass
@@ -119,16 +123,14 @@ class TransmissionTorrent(Torrent):
 
 		pass
 	
-	
-
 def main():
 	logging.basicConfig(filename = os.path.expanduser(LOGFILE), level = LOGLEVEL, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 	logging.info("== STARTING ==")
-
-	t = TransmissionTorrent()
-	t.createMediaLibraryCopy()
 	
-	pass
+	t = TransmissionTorrent()
+	if not t.name:
+		return
+	t.createMediaLibraryCopy()
 
 if __name__ == "__main__":
 	sys.exit(main())
