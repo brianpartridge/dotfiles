856196b61651cdd8070e9b844eae6fd8145ada94
Brian Partridge <brianpartridge@gmail.com>
2016-01-24 12:57:04 -0500

    Only add new watchlist items to wishlist.
    - Save date_added of newest watchlist item on each run.
    - Only add items to wishlist if they were added after the previously
      saved date.
    - Prevents issues where an item remains on watchlist, but has already
      been removed from wishlist.

--- common/bin/watchlist-to-wishlist.rb
+++ common/bin/watchlist-to-wishlist.rb
@@ -5,6 +5,12 @@ require 'hdbits'
 require 'imdb_watchlist'
 require 'set'
 
+$conf_dir = '~/Dropbox/conf/'
+
+def conf_file(filename)
+    File.expand_path($conf_dir + filename)
+end
+
 def first_line_from_file(path)
     File.readlines(File.expand_path(path)).first
 end
@@ -24,21 +30,49 @@ def ids_not_on_wishlist(watchlist_items, wishlist_items)
     watchlist_ids.subtract(wishlist_ids)
 end
 
+def new_hdbits
+    user = first_line_from_file(conf_file('hdbits-username'))
+    passkey = first_line_from_file(conf_file('hdbits-passkey'))
+    HDBits.new(user, passkey)
+end
+
+def new_watchlist
+    watchlist_url = first_line_from_file(conf_file('imdb-watchlist-url'))
+    IMDbWatchlist.new(watchlist_url)
+end
+
+def last_watchlist_update_time
+    path = conf_file('imdb-watchlist-last-update')
+    return Time.at(0) unless File.exists? path
+    Time.parse(first_line_from_file(path))
+end
+
+def save_last_watchlist_update_time(time)
+    path = conf_file('imdb-watchlist-last-update')
+    File.open(path, 'w') { |f| f.write(time.to_s) }
+end
+
+def recent_watchlist_items(watchlist)
+    time = last_watchlist_update_time
+    watchlist.items.select { |i| i.date_added > time }.sort_by { |i| i.date_added }
+end
+
 def run!
-    user = first_line_from_file('~/Dropbox/conf/hdbits-username')
-    passkey = first_line_from_file('~/Dropbox/conf/hdbits-passkey')
-    hdbits = HDBits.new(user, passkey)
+    hdbits = new_hdbits
     wishlist_items = hdbits.wishlist
 
-    watchlist_url = first_line_from_file('~/Dropbox/conf/imdb-watchlist')
-    watchlist = IMDbWatchlist.new(watchlist_url)
+    watchlist = new_watchlist
+    watchlist_items = recent_watchlist_items(watchlist)
+    return if watchlist_items.empty?
 
-    imdb_ids = ids_not_on_wishlist(watchlist.items, wishlist_items)
-    puts "--- Adding #{ imdb_ids.count } ---"
-    imdb_ids.each do |id| 
-        puts id
-        puts "ERROR: unable toadd #{ id } to wishlist" unless hdbits.add_to_wishlist(id)
+    ids_to_add = ids_not_on_wishlist(watchlist_items, wishlist_items)
+    items_to_add = watchlist_items.select { |i| ids_to_add.member? i.id }
+    puts "--- Adding #{ items_to_add.count } ---"
+    items_to_add.each do |i| 
+        puts i
+        puts "ERROR: unable to add #{ i.title } to wishlist" unless hdbits.add_to_wishlist(i.id)
     end
+    save_last_watchlist_update_time(watchlist_items.last.date_added)
 end
 
 run!
