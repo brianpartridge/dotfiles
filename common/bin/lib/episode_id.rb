747399068d4d9d4b40101c9108e6f5ee761dcfbc
Brian Partridge <brianpartridge@gmail.com>
2017-09-23 22:42:34 -0400

    Add support for miniseries episodes.

--- common/bin/lib/episode_id.rb
+++ common/bin/lib/episode_id.rb
@@ -1,26 +1,37 @@
 # Model to simplify comparison of S#E#-based episodes
 class EpisodeID 
-  attr_reader :season, :episode
-  def initialize(season, episode)
+  attr_reader :season, :episode, :is_miniseries
+  def initialize(season, episode, is_miniseries = false)
     @season = season.to_i
     @episode = episode.to_i
+    @is_miniseries = is_miniseries
   end
  
   def to_s
+    return "Part%02i" % [@episode] if @is_miniseries
     "S%02iE%02i" % [@season, @episode]
   end
 
   def self.from_s(s)
+    match_data = s.match(/^Part(?<episode>\d+)$/)
+    return EpisodeID.new(1, match_data['episode'], true) if match_data
+    
     match_data = s.match(/^S(?<season>\d+)E(?<episode>\d+)$/)
-    return nil unless match_data
-    EpisodeID.new(match_data['season'], match_data['episode'])
+    return EpisodeID.new(match_data['season'], match_data['episode']) if match_data
+    
+    nil
   end
  
   def self.from_release(name)
     sanitized_name = name.gsub(/h.264/i, '').gsub(/720p/i, '').gsub(/1080p/i, '')
+
+    match_data = sanitized_name.match(/^(?<series>.+)[\._ \-][Pp]art(?<episode>\d{2})[\._ \-]/)
+    return EpisodeID.new(1, match_data['episode'], true) if match_data
+    
     match_data = sanitized_name.match(/^(?<series>.+)[\._ \-][Ss]?(?<season>\d+)[\._ \-]?[EeXx](?<episode>\d{2})[\._ \-]/)
-    return nil unless match_data
-    EpisodeID.new(match_data['season'], match_data['episode'])
+    return EpisodeID.new(match_data['season'], match_data['episode']) if match_data
+
+    nil
   end
   
   def <(ep)
--- common/bin/spec/episode_id_spec.rb
+++ common/bin/spec/episode_id_spec.rb
@@ -14,10 +14,21 @@ describe 'EpisodeID' do
     EpisodeID.new('2', '1')
   end
 
+  def p1
+    EpisodeID.new('1', '1', true)
+  end
+
+  def p2
+    EpisodeID.new('1', '2', true)
+  end
+
   describe 'initialize' do
     it 'stores attributes' do
       expect(e12.season).to eq(1)
       expect(e12.episode).to eq(2)
+
+      expect(p2.season).to eq(1)
+      expect(p2.episode).to eq(2)
     end
 
     it 'handles string input' do
@@ -31,6 +42,8 @@ describe 'EpisodeID' do
       expect(e11).to eq(e11)
       expect(e12).to eq(e12)
       expect(e21).to eq(e21)
+      expect(e11).to eq(p1)
+      expect(e12).to eq(p2)
     end
 
     it 'handles relative ordering' do
@@ -59,6 +72,8 @@ describe 'EpisodeID' do
       expect(e11.to_s).to eq('S01E01')
       expect(e12.to_s).to eq('S01E02')
       expect(e21.to_s).to eq('S02E01')
+
+      expect(p2.to_s).to eq('Part02')
     end
   end
 
@@ -67,10 +82,12 @@ describe 'EpisodeID' do
       expect(EpisodeID.from_s('S01E01')).to eq(e11)
       expect(EpisodeID.from_s('S1E2')).to eq(e12)
       expect(EpisodeID.from_s('S000002E000001')).to eq(e21)
+      expect(EpisodeID.from_s('Part02')).to eq(p2)
 
       expect(EpisodeID.from_s('s01e01')).to eq(nil)
       expect(EpisodeID.from_s('1xE01')).to eq(nil)
       expect(EpisodeID.from_s('101')).to eq(nil)
+      expect(EpisodeID.from_s('PartTwo')).to eq(nil)
     end
   end
   
@@ -81,6 +98,7 @@ describe 'EpisodeID' do
       expect(EpisodeID.from_release('Series.1x01.720p')).to eq(e11)
       expect(EpisodeID.from_release('123.S01E01.720p')).to eq(e11)
       expect(EpisodeID.from_release('Series.S01E01.123')).to eq(e11)
+      expect(EpisodeID.from_release('Series.Part02.123')).to eq(p2)
 
       expect(EpisodeID.from_release('Series.101.720p')).to eq(nil)
       expect(EpisodeID.from_release('S01E01')).to eq(nil)
